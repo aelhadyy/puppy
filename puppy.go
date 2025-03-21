b3723049d7ac511d41d1728b85a524003897f858
Ahmed Elhady <ahmedelhady.dev@gmail.com>
2025-03-21 20:34:25 +0200

    added new tag

--- puppy.go
+++ puppy.go
@@ -1,6 +1,10 @@
 package puppy
 
-import "github.com/aelhadyy/dog"
+import (
+	"fmt"
+
+	"github.com/aelhadyy/dog"
+)
 
 func Bark() string {
 	return "Woof!"
@@ -16,4 +20,8 @@ func BigBark() string {
 
 func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
+}
+
+func Fromv12() {
+	fmt.Println("I'm from v1.2.0")
 }
